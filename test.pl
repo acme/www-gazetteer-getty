59e4c73689a3d9fb2f6e51f12224baac73ee58f2
Leon Brocard <acme@astray.com>
2009-06-23 19:50:51 +0100

    perltidy

--- test.pl
+++ test.pl
@@ -2,25 +2,31 @@
 use strict;
 use Test::More tests => 4;
 
-BEGIN { use_ok( 'WWW::Gazetteer' ); }
+BEGIN { use_ok('WWW::Gazetteer'); }
 
 my $g = WWW::Gazetteer->new('getty');
-is_deeply($g->find("London", "United Kingdom")->[0], {
-  longitude => "-0.167",
-  latitude => "51.500",
-  city => 'London',
-  country => 'United Kingdom',
-});
-is_deeply($g->find("London", "UK")->[0], {
-  longitude => "-0.167",
-  latitude => "51.500",
-  city => 'London',
-  country => 'United Kingdom',
-});
-my $nice = $g->find("Nice", "France")->[0];
-is_deeply($nice, {
-  longitude => "7.267",
-  latitude => "43.700",
-  city => 'Nice',
-  country => 'France',
-});
+is_deeply(
+    $g->find( "London", "United Kingdom" )->[0],
+    {   longitude => "-0.167",
+        latitude  => "51.500",
+        city      => 'London',
+        country   => 'United Kingdom',
+    }
+);
+is_deeply(
+    $g->find( "London", "UK" )->[0],
+    {   longitude => "-0.167",
+        latitude  => "51.500",
+        city      => 'London',
+        country   => 'United Kingdom',
+    }
+);
+my $nice = $g->find( "Nice", "France" )->[0];
+is_deeply(
+    $nice,
+    {   longitude => "7.267",
+        latitude  => "43.700",
+        city      => 'Nice',
+        country   => 'France',
+    }
+);
